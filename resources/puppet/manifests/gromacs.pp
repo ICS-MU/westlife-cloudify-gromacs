a5eec7aebdee694d22405498099ffe3ecf29d36b
Vlastimil Holer <holer@ics.muni.cz>
2017-02-19 21:09:04 +0100

    Automatically setup CUDA if we have NVIDIA ($::has_nvidia_gpu)

--- resources/puppet/manifests/gromacs.pp
+++ resources/puppet/manifests/gromacs.pp
@@ -2,7 +2,7 @@ include ::gromacs
 
 # setup CUDA only if release specified
 $cuda_release = hiera('cuda::release')
-if ("${cuda_release}" != '') {
+if ("${cuda_release}" != '') and ($::has_nvidia_gpu == true) {
   warning("cuda_release: ${cuda_release}")
   include ::cuda
 }
