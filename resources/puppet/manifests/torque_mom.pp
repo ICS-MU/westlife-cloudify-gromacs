d357cbc6d1a6ea11f7688d3049a37b143eb88f0d
Vlastimil Holer <holer@ics.muni.cz>
2017-02-19 21:09:57 +0100

    Torque MOM only with 1 CPU available (to run all jobs exclusively)

--- resources/puppet/manifests/torque_mom.pp
+++ resources/puppet/manifests/torque_mom.pp
@@ -9,7 +9,8 @@ if ($::cloudify_ctx_type == 'node-instance') {
 } elsif ($::cloudify_ctx_type == 'relationship-instance') {
   $_node_fqdn = regsubst($::fqdn, '\.', '_', 'G')
   ctx { "torque_node_${_node_fqdn}":
-    value => $facts['processors']['count'],
+    value => 1,  # we want the node exclusively
+    #value => $facts['processors']['count'],
     side  => 'target',
   }
 
